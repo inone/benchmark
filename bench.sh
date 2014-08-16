83732c2ff46786577bcc687836fdf43a456cbc52
Ismail Fazal <ismailfazal@gmail.com>
2014-08-16 17:05:12 +0530

    Create bench.sh
    ONly one speedtest source cachefly

---
+++ bench.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+tram=$( free -m | awk 'NR==2 {print $2}' )
+swap=$( free -m | awk 'NR==4 {print $2}' )
+up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
+
+echo "CPU model : $cname"
+echo "Number of cores : $cores"
+echo "CPU frequency : $freq MHz"
+echo "Total amount of ram : $tram MB"
+echo "Total amount of swap : $swap MB"
+echo "System uptime : $up"
+
+cachefly=$( wget -O /dev/null http://cachefly.cachefly.net/100mb.test 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
+echo "Download speed from CacheFly: $cachefly "
+
+io=$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' )
+echo "I/O speed : $io"
